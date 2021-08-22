5d0ad6f01630cd51382d8cc2f3f2705cf6c9f198
Uros Urosevic <urosevic1986@gmail.com>
2021-08-22 13:57:27 +0200

    use d3-time-format to date values from strings to JavaScript Date objects

--- d3-corona-line-chart/src/useData.js
+++ d3-corona-line-chart/src/useData.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'https://cdn.skypack.dev/react';
-import { csv } from 'https://cdn.skypack.dev/d3';
+import { csv, timeParse } from 'https://cdn.skypack.dev/d3';
 
 const csvUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
 
 const sum = (accumulator, currentValue) => accumulator + currentValue;
+const parseDate = timeParse('%d/%m/%y');
 
 const transform = rawData => {
   // console.log(rawData.columns);
-  const dates = rawData.columns.slice(4);
-  return dates.map(date => {
-    const deathTotal = rawData.map(d => +d[date]).reduce(sum, 0);
+  const days = rawData.columns.slice(4);
+  return days.map(day => {
+    const deathTotal = rawData.map(d => +d[day]).reduce(sum, 0);
     return {
-      date,
+      date: parseDate(day),
       deathTotal
     };
   });  
