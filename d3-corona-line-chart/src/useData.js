94c33c2042c2af52e979841e732adb52f1ecbb66
Uros Urosevic <urosevic1986@gmail.com>
2021-08-22 13:49:30 +0200

    transform dataset to unpivot from columns to rows

--- d3-corona-line-chart/src/useData.js
+++ d3-corona-line-chart/src/useData.js
@@ -3,6 +3,20 @@ import { csv } from 'https://cdn.skypack.dev/d3';
 
 const csvUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
 
+const sum = (accumulator, currentValue) => accumulator + currentValue;
+
+const transform = rawData => {
+  // console.log(rawData.columns);
+  const dates = rawData.columns.slice(4);
+  return dates.map(date => {
+    const deathTotal = rawData.map(d => +d[date]).reduce(sum, 0);
+    return {
+      date,
+      deathTotal
+    };
+  });  
+};
+
 export const useData = () => {
   const [data, setData] = useState(null);
 
@@ -11,7 +25,9 @@ export const useData = () => {
   // }
 
   useEffect(() => {
-    csv(csvUrl).then(setData);
+    csv(csvUrl).then(rawData => {
+      setData(transform(rawData));
+    });
   }, [])
 
   return data;
