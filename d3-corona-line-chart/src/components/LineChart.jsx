c8217bddc481a068a3e46a00207b352ecb1c2ce0
Uros Urosevic <urosevic1986@gmail.com>
2021-08-22 23:34:28 +0200

    renders very basic Line chart using d3-shape's line method

--- d3-corona-line-chart/src/components/LineChart.jsx
+++ d3-corona-line-chart/src/components/LineChart.jsx
@@ -1,17 +1,28 @@
 import React from 'https://cdn.skypack.dev/react';
-import { scaleTime, extent, scaleLinear, max } from 'https://cdn.skypack.dev/d3';
+import { scaleTime, extent, scaleLinear, max, line } from 'https://cdn.skypack.dev/d3';
+
+const xValue = d => d.date;
+const yValue = d => d.deathTotal;
 
 export const LineChart = ({ data, width, height }) => {
   const xScale = scaleTime()
-    .domain(extent(data, d => d.date))
+    .domain(extent(data, xValue))
     .range([0, width]);
 
   const yScale = scaleLinear()
-    .domain([0, max(data, d => d.deathTotal)])
+    .domain([0, max(data, yValue)])
     .range([height, 0]);
 
-  console.log(xScale.domain());
-  console.log(yScale.domain());
+  // console.log(xScale.domain());
+  // console.log(yScale.domain());
 
-  return <svg width={width} height={height} />
+  const lineGenerator = line()
+    .x(d => xScale(xValue(d)))
+    .y(d => yScale(yValue(d)))
+
+  return (
+    <svg width={width} height={height}> 
+      <path d={lineGenerator(data)} />
+    </svg>
+  );  
 }
--- d3-corona-line-chart/src/useData.js
+++ d3-corona-line-chart/src/useData.js
@@ -4,13 +4,12 @@ import { csv, timeParse } from 'https://cdn.skypack.dev/d3';
 const csvUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
 
 const sum = (accumulator, currentValue) => accumulator + currentValue;
-const parseDate = timeParse('%d/%m/%y');
+const parseDay = timeParse('%m/%d/%y');
 
 const transform = rawData => {
-  // console.log(rawData.columns);
   const days = rawData.columns.slice(4);
   return days.map(day => ({
-    date: parseDate(day),
+    date: parseDay(day),
     deathTotal: rawData.map(d => +d[day]).reduce(sum, 0)
   }));  
 };
