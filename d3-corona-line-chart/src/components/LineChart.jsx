dcaf4c49c400fb9883d052e290e55d61e54d1adc
Uros Urosevic <urosevic1986@gmail.com>
2021-08-22 15:41:01 +0200

    replace min and max in xScale with more concise extent helper function

--- d3-corona-line-chart/src/components/LineChart.jsx
+++ d3-corona-line-chart/src/components/LineChart.jsx
@@ -1,9 +1,9 @@
 import React from 'https://cdn.skypack.dev/react';
-import { scaleTime } from 'https://cdn.skypack.dev/d3';
+import { scaleTime, extent } from 'https://cdn.skypack.dev/d3';
 
 export const LineChart = ({ data, width, height }) => {
   const xScale = scaleTime()
-    .domain([min(data, d => d.date), max(data, d => d.date)])
+    .domain(extent(data, d => d.date))
     .range([0, width]);
 
   console.log(xScale.domain());
