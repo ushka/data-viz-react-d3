66e61a8b98add4c2a78a753b6d94f8cf06b07a10
Uros Urosevic <urosevic1986@gmail.com>
2021-08-22 20:34:39 +0200

    setup yScale for Line chart

--- d3-corona-line-chart/src/components/LineChart.jsx
+++ d3-corona-line-chart/src/components/LineChart.jsx
@@ -1,12 +1,17 @@
 import React from 'https://cdn.skypack.dev/react';
-import { scaleTime, extent } from 'https://cdn.skypack.dev/d3';
+import { scaleTime, extent, scaleLinear, max } from 'https://cdn.skypack.dev/d3';
 
 export const LineChart = ({ data, width, height }) => {
   const xScale = scaleTime()
     .domain(extent(data, d => d.date))
     .range([0, width]);
 
+  const yScale = scaleLinear()
+    .domain([0, max(data, d => d.deathTotal)])
+    .range([height, 0]);
+
   console.log(xScale.domain());
+  console.log(yScale.domain());
 
   return <svg width={width} height={height} />
 }
