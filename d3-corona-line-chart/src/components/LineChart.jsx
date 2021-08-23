317773faccd1aa73692e5f36ca0032e673e20bbf
Uros Urosevic <urosevic1986@gmail.com>
2021-08-23 12:23:11 +0200

    applies d3 margin convention to Line chart to provide suitable margins around the chart making space for marks and labels

--- d3-corona-line-chart/src/components/LineChart.jsx
+++ d3-corona-line-chart/src/components/LineChart.jsx
@@ -3,18 +3,24 @@ import { scaleTime, extent, scaleLinear, max, line } from 'https://cdn.skypack.d
 
 const xValue = d => d.date;
 const yValue = d => d.deathTotal;
+const margin = {
+  top: 40,
+  right: 40,
+  bottom: 40,
+  left: 90
+};
 
 export const LineChart = ({ data, width, height }) => {
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
   const xScale = scaleTime()
     .domain(extent(data, xValue))
-    .range([0, width]);
+    .range([0, innerWidth]);
 
   const yScale = scaleLinear()
     .domain([0, max(data, yValue)])
-    .range([height, 0]);
-
-  // console.log(xScale.domain());
-  // console.log(yScale.domain());
+    .range([innerHeight, 0]);
 
   const lineGenerator = line()
     .x(d => xScale(xValue(d)))
@@ -22,12 +28,14 @@ export const LineChart = ({ data, width, height }) => {
 
   const markerLineY = yScale(100000);
   const markerLineX1 = 0;
-  const markerLineX2 = width;
+  const markerLineX2 = innerWidth;
 
   return (
     <svg width={width} height={height}> 
-      <line className="markerLine" x1={markerLineX1} y1={markerLineY} x2={markerLineX2} y2={markerLineY} />
-      <path d={lineGenerator(data)} />
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <line className="markerLine" x1={markerLineX1} y1={markerLineY} x2={markerLineX2} y2={markerLineY} />
+        <path d={lineGenerator(data)} />
+      </g>
     </svg>
   );  
 }
