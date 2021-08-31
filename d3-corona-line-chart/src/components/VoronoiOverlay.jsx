8745064e307d8243e4aeceb30e4e2cc563d68e52
Uros Urosevic <urosevic1986@gmail.com>
2021-08-31 20:20:55 +0200

    extends Voronoi overlay to go to the edge so tooltips appear when you hover past the right edge of the chart

--- d3-corona-line-chart/src/components/VoronoiOverlay.jsx
+++ d3-corona-line-chart/src/components/VoronoiOverlay.jsx
@@ -15,7 +15,7 @@ export const VoronoiOverlay = ({ innerWidth, innerHeight, allData, lineGenerator
       lineGenerator.y()(d)
     ]);
     const delaunay = Delaunay.from(points);
-    const voronoi = delaunay.voronoi([0, 0, innerWidth, innerHeight]);
+    const voronoi = delaunay.voronoi([0, 0, innerWidth + 100, innerHeight]);
     return (
       <g className="voronoi">
         {points.map((point, i) => (
